e1086a54b1db2baed0b59a291dc58905e471fe71
aliabdo6 <aliellool202020@gmail.com>
2023-12-29 20:52:54 +0200

    'up'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,42 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import {
+  Inter,
+  Space_Grotesk,
+} from "next/font/google";
 
-export const metadata = {
+const inter = Inter({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+  variable: "--font-inter",
+});
+
+const spacegrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+import type { Metadata } from "next";
+export const metadata: Metadata = {
   title: "Dev Stack OverFlow",
+  description:
+    " A community-driven platform for asking and answering programming questions . Get help , share knowledge , and collaborate with developers from around the world . Explore topics in web development , mobile app development , algorithms , data structures , and more .  ",
+
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -12,9 +45,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary:
+            "bg-blue-500 hover:bg-blue-600",
+          footerActionLink:
+            "text-blue-500 hover:text-blue-600",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body
+          className={` ${inter.variable} ${spacegrotesk.variable} `}
+        >
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
