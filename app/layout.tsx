6c9c0bcf3ed354ee4d2a0466bd4ff006c7642600
aliabdo6 <aliellool202020@gmail.com>
2024-02-11 16:50:16 +0200

    'up'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,65 @@
-import React from 'react';
-import { ClerkProvider } from '@clerk/nextjs';
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
-import { Inter, Space_Grotesk } from 'next/font/google';
-import type { Metadata } from 'next';
-import './globals.css';
-import ThemeProvider from '@/context/ThemeProvider';
+import {
+  Inter,
+  Space_Grotesk,
+} from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import ThemeProvider from "@/context/ThemeProvider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+  variable: "--font-inter",
 });
 const spaceGrotest = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk'
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
 });
 
 export const metadata: Metadata = {
-  title: 'DevFlow',
+  title: "DevFlow",
   description:
-    'A community driven platform for developers asking and answering questions.Get help and answers to your questions about anything related to programming and software development from the best developers on the internet.',
-  icons: '/assets/images/site-logo.svg'
+    "A community driven platform for developers asking and answering questions.Get help and answers to your questions about anything related to programming and software development from the best developers on the internet.",
+  icons: "/assets/images/site-logo.svg",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+      <body
+        className={`${inter.variable} ${spaceGrotest.variable}`}
+      >
         <ClerkProvider
           appearance={{
             elements: {
-              formButtonPrimary: 'primary-gradient',
-              footerActionLink: 'primary-text-gradient hover:text-primary-500'
-            }
+              formButtonPrimary:
+                "primary-gradient",
+              footerActionLink:
+                "primary-text-gradient hover:text-primary-500",
+            },
           }}
         >
-          <ThemeProvider>{children}</ThemeProvider>
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
         </ClerkProvider>
       </body>
     </html>
