25c9134f5bc96bef6b1f31bfbfe741294841477d
aliabdo6 <aliellool202020@gmail.com>
2024-02-18 15:51:28 +0200

    'add some ui in home page'

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,80 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: 1,
+    title: "Introduction to Machine Learning",
+    tags: [
+      { _id: 1, name: "machine learning" },
+      { _id: 2, name: "artificial intelligence" },
+    ],
+    author: "John Doe",
+    upvotes: 25,
+    views: 500,
+    answers: 5,
+    createdAt: "2024-02-18T08:30:00.000Z",
+  },
+  {
+    _id: 2,
+    title: "React vs Vue: Which One to Choose?",
+    tags: [
+      { _id: 3, name: "javascript" },
+      { _id: 4, name: "react" },
+      { _id: 5, name: "vue" },
+    ],
+    author: "Jane Smith",
+    upvotes: 40,
+    views: 800,
+    answers: 8,
+    createdAt: "2024-01-15T15:45:00.000Z",
+  },
+  {
+    _id: 3,
+    title:
+      "Data Structures and Algorithms Basics",
+    tags: [
+      { _id: 6, name: "data structures" },
+      { _id: 7, name: "algorithms" },
+    ],
+    author: "Alex Johnson",
+    upvotes: 15,
+    views: 300,
+    answers: 3,
+    createdAt: "2024-03-05T10:20:00.000Z",
+  },
+  {
+    _id: 4,
+    title: "Getting Started with Python",
+    tags: [
+      { _id: 8, name: "python" },
+      { _id: 9, name: "programming" },
+    ],
+    author: "Sarah Brown",
+    upvotes: 30,
+    views: 600,
+    answers: 6,
+    createdAt: "2024-04-10T09:00:00.000Z",
+  },
+  {
+    _id: 5,
+    title: "Understanding Blockchain Technology",
+    tags: [
+      { _id: 10, name: "blockchain" },
+      { _id: 11, name: "cryptocurrency" },
+    ],
+    author: "Michael Clark",
+    upvotes: 20,
+    views: 400,
+    answers: 4,
+    createdAt: "2024-05-20T14:15:00.000Z",
+  },
+];
 
 export default function Home() {
   return (
@@ -39,6 +110,25 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+      <div className=" mt-10 flex w-full flex-col gap-6  ">
+        {/* {looping through Questions} */}
+        {questions.length > 0 ? (
+          questions.map(
+            (question) => "QustionCard"
+          )
+        ) : (
+          <NoResult
+            title="Thers is no Questions to show 👀"
+            description="Be the first to break the silence ! 🤞🔔
+        Ask a Question and kickstart the
+        discussion . our Query could be the next
+        big thing others learn from . Get
+        involved✨✨"
+            link="/ask-question"
+            linkTitle="ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
