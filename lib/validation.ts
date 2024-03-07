6a420204cd9b71510131f3d1df0b416613ef4d64
aliabdo6 <aliellool202020@gmail.com>
2024-03-07 18:34:40 +0200

    add form

---
+++ lib/validation.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+export const QuestionsSchema = z.object({
+  title: z.string().min(5, "Title is too short"),
+  description: z
+    .string()
+    .min(100, "Description is too short"),
+  tags: z
+    .array(z.string())
+    .min(1, "Please add at least one tag"),
+});
